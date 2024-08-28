70e4827e39202b769822fdf907def0729761d9c4
Azimjon <azimjonbieber@gmail.com>
2024-08-28 15:29:31 +0500

    repair login code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NextPage from "./components/NextPage";
-import Login from "./components/login/Login";
+import Login from "./components/auth/Login";
 
 function App() {
   return (
